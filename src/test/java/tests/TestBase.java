7d71cbe80618d5dd537e9906b671b89bd64773a7
3007ver <veronika.shyshlo@gmail.com>
2023-07-21 23:56:30 +0300

    init commit

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -28,7 +28,7 @@ public class TestBase {
         Configuration.browserCapabilities = ops;
         Configuration.baseUrl = System.getProperty("baseUrl", "https://simplewine.ru/");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        String[] browser = System.getProperty("browser", "chrome:100.0").split(":");
+        String[] browser = System.getProperty("browser", "chrome:115.0").split(":");
         Configuration.browser = browser[0];
         Configuration.browserVersion = browser[1];
         Configuration.pageLoadStrategy = "eager";
